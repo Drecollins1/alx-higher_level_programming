ec9bb8773fdaf219ad5a8e6f7def49b9240b2475
Drecollins1 <adarecollins@gmail.com>
2023-10-05 09:06:02 -0700

    python modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ print the sum, subtraction, multiplication and division of 10 and 5"""
-from calculator_1 import add,sub,mul,div
 if  __name__ == "__main__":
-
+from calculator_1 import add,sub,mul,div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
